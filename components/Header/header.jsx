b6d04e392e573b9ba7c7938249d00add37f7ca1f
Travis Alexander <talexa38@charlotte.edu>
2026-03-08 03:53:01 -0400

    Add Header component with basic structure

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import './Header.css';
 
+class Header extends React.Component 
+{
+    constructor(props) 
+    {
+        super(props);
+    }
+
+    render() 
+    {
+        return (
+            <div className="container Header">
+                <div className="header-title">
+                    <span className="header-icon"></span> Header
+                </div>
+                <div className="header-stripe"></div>
+            </div>
+        );
+    }
+}
+
+export default Header;
